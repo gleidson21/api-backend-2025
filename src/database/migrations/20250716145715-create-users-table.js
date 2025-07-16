45dc1de30c670c253d1d97b2e76c007d95141847
gleidson21 <gledsonalcantara4@gamil.com>
2025-07-16 17:56:07 -0400

    pronto

--- src/database/migrations/20250716145715-create-users-table.js
+++ src/database/migrations/20250716145715-create-users-table.js
@@ -1,40 +1,43 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('users', {
-      id: {
-        primaryKey: true,
-        allowNull: false,
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
-      },
-      password_hash: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-    });
-  },
+export const up = async (sequelize) => {
+  const queryInterface = sequelize.getQueryInterface();
+  const Sequelize = sequelize.constructor;
+  
+  await queryInterface.createTable('users', {
+    id: {
+      primaryKey: true,
+      allowNull: false,
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password_hash: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  });
+};
 
-  async down(queryInterface) {
-    await queryInterface.dropTable('users');
-  }
+export const down = async (sequelize) => {
+  const queryInterface = sequelize.getQueryInterface();
+  
+  await queryInterface.dropTable('users');
 };
