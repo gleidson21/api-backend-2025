592b68c4ab89fb885c05e2911a0993d364cf4998
gleidson21 <gledsonalcantara4@gamil.com>
2025-07-16 16:31:33 -0400

    agora vai sim

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 // src/database/index.js
+import 'dotenv/config'
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database.js';
 import User from '../app/models/User.js';
-
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 const connection = new Sequelize(databaseConfig.url, databaseConfig);
 
 User.init(connection);
