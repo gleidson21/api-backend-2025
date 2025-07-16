b0245e3af8771b3192f83c591fcda29febae86c6
gleidson21 <gledsonalcantara4@gamil.com>
2025-07-16 17:12:04 -0400

    agora sim vai da certo

--- migrate.js
+++ migrate.js
@@ -1,8 +1,7 @@
 import { Sequelize } from 'sequelize';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { Umzug } from 'umzug';
-import { SequelizeStorage } from 'umzug/sequelize';
+import { Umzug, SequelizeStorage } from 'umzug';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
