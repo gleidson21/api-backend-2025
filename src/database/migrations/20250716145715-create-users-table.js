3d7926f79ed4003a0340f9d3930a7e2f1461412d
gleidson21 <gledsonalcantara4@gamil.com>
2025-07-16 17:16:39 -0400

    agora vai em nome de jesus

--- src/database/migrations/20250716145715-create-users-table.js
+++ src/database/migrations/20250716145715-create-users-table.js
@@ -1,52 +1,38 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
-export default {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('users', {
+export const up = async (queryInterface, Sequelize) => {
+  await queryInterface.createTable('users', {
+    id: {
+      primaryKey: true,
+      allowNull: false,
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password_hash: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  });
+};
 
-
-      id: {
-        primaryKey: true,
-        allowNul: false,
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4
-
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
-      },
-      password_hash: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-
-
-
-    });
-
-  },
-
-  async down(queryInterface) {
-
-    await queryInterface.dropTable('users')
-
-  }
-
-}
+export const down = async (queryInterface) => {
+  await queryInterface.dropTable('users');
+};
