5288148e2a449e3ad166ff6dba1c3c15b75e6d7d
gleidson21 <gledsonalcantara4@gamil.com>
2025-07-17 18:31:35 -0400

    agora sim

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,10 @@ import authenticateToken from './app/middlewares/authenticateToken.js';
 import isAdmin from './app/middlewares/isAdmin.js'; // Certifique-se de que este caminho está correto
 import { Router } from "express";
 import SessionController from './app/controllers/SessionController.js'; // Certifique-se de que este caminho está correto
-const sessionControllerInstance = new SessionController();
 const router = new Router();
 
 router.post('/users', usercontroller.store);
-router.post('/login', sessionControllerInstance.store);
+router.post('/login', SessionController.store);
 
 // Rota para deletar um usuário (agora protegida por authenticateToken E isAdmin)
 router.delete('/users/:id', authenticateToken, isAdmin, usercontroller.delete);
