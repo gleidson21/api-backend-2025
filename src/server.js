012b97232b9a9747c613583ad7225924a5afbfbc
gleidson21 <gledsonalcantara4@gamil.com>
2025-07-20 16:45:15 -0400

    gjkjkjkjkf

--- src/server.js
+++ src/server.js
@@ -1,27 +1,11 @@
 import 'dotenv/config'; // Garante que as variáveis de ambiente sejam carregadas
 import express from 'express';
-import path from 'path'; // Importa o módulo 'path'
-import { fileURLToPath } from 'url'; // Importa para lidar com __dirname em módulos ES
 
 import app from './app.js'; // Importa a instância do Express já configurada de app.js
 import userRoute from './routes.js'; // Importa suas rotas
 import './database/index.js'; // ESSENCIAL: Importa e executa a conexão com o DB e inicializa os modelos!
 
-// Definindo __filename e __dirname para módulos ES
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 try {
-    // --- NOVO: Middleware para servir arquivos estáticos da pasta 'public' ---
-    // Como 'public' está dentro de 'src' (onde 'server.js' está), o caminho é 'public'.
-    app.use(express.static(path.join(__dirname, 'public')));
-    console.log('Servindo arquivos estáticos de src/public');
-
-    // Configura para servir arquivos estáticos da pasta 'assets' (se ela existir na raiz do projeto)
-    // Se 'assets' estiver na raiz do projeto e 'server.js' estiver em 'src',
-    // precisamos voltar um nível (..) para acessá-la.
-    app.use('/assets', express.static(path.join(__dirname, '..', 'assets')));
-    console.log('Servindo arquivos estáticos de ../assets');
 
     // Usa as rotas no aplicativo Express
     app.use(userRoute);
